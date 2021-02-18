e0125b963c4cb2549ec44162d1fc049d146a4d90
vzhomeexperiments <vz.home.experiments@gmail.com>
2021-02-18 21:54:51 +0100

    Update _PushToMT4Prod.bat

--- _PushToMT4Prod.bat
+++ _PushToMT4Prod.bat
@@ -4,16 +4,16 @@ rem Use when you need to publish all files to All Terminals
 @echo off
 setlocal enabledelayedexpansion
 
-set SOURCE_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\FALCON_A"
-set DEST_DIR1="C:\Program Files (x86)\FxPro - Terminal1\MQL4\Experts\03_FALCON_A"
-set DEST_DIR2="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Experts\03_FALCON_A"
-set DEST_DIR3="C:\Program Files (x86)\FxPro - Terminal3\MQL4\Experts\03_FALCON_A"
-set DEST_DIR4="C:\Program Files (x86)\FxPro - Terminal4\MQL4\Experts\03_FALCON_A"
+set SOURCE_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\The-Falcon"
+set DEST_DIR1="C:\Program Files (x86)\FxPro - Terminal1\MQL4\Experts\The-Falcon"
+set DEST_DIR2="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Experts\The-Falcon"
+set DEST_DIR3="C:\Program Files (x86)\FxPro - Terminal3\MQL4\Experts\The-Falcon"
+set DEST_DIR4="C:\Program Files (x86)\FxPro - Terminal4\MQL4\Experts\The-Falcon"
 
-ROBOCOPY %SOURCE_DIR% %DEST_DIR1% Falcon_A.mq4
-ROBOCOPY %SOURCE_DIR% %DEST_DIR2% Falcon_A.mq4
-ROBOCOPY %SOURCE_DIR% %DEST_DIR3% Falcon_A.mq4
-ROBOCOPY %SOURCE_DIR% %DEST_DIR4% Falcon_A.mq4
+ROBOCOPY %SOURCE_DIR% %DEST_DIR1% *.mq4
+ROBOCOPY %SOURCE_DIR% %DEST_DIR2% *.mq4
+ROBOCOPY %SOURCE_DIR% %DEST_DIR3% *.mq4
+ROBOCOPY %SOURCE_DIR% %DEST_DIR4% *.mq4
 
 
 
