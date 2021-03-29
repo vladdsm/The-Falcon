80ca1871f96933039fe468eff54d4d14971dd601
vzhomeexperiments <vz.home.experiments@gmail.com>
2021-03-29 23:19:12 +0200

    support Environmental Vars

--- _PushToMT4Dev.bat
+++ _PushToMT4Dev.bat
@@ -5,9 +5,9 @@ rem Use in case some content needs to be replaced (reverted from Version Control
 setlocal enabledelayedexpansion
 
 :: Source Directory where Version Control Repository is located
-set SOURCE_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\The-Falcon"
+set SOURCE_DIR="%PATH_DSS_Repo%\The-Falcon"
 :: Destination Directory where Expert Advisor is located
-set DEST_DIR="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Experts\The-Falcon"
+set DEST_DIR="%PATH_T2_E%\The-Falcon"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR% *.mq4
 
---
+++ setupEV.bat
@@ -0,0 +1,27 @@
+::# ----------------------------------------------------------------------------------------
+::# Environmental Variables for Lazytrade System
+::# ----------------------------------------------------------------------------------------
+::# User Environmental Variables
+::# Check in: Computer Properties -> Advanced System Settings -> Environmental Variables -> User Variables
+
+::# Customize path to the user folder where all relevantrepositories should be located
+setx PATH_DSS_Repo "%USERPROFILE%\Documents\000_TradingRepo"
+
+::# Customize path to the user folders with Trading Platforms
+setx PATH_T1 "C:\Program Files (x86)\FxPro - Terminal1\MQL4\Files"
+setx PATH_T1_E "C:\Program Files (x86)\FxPro - Terminal1\MQL4\Experts"
+setx PATH_T1_I "C:\Program Files (x86)\FxPro - Terminal1\MQL4\Include"
+
+setx PATH_T2 "C:\Program Files (x86)\FxPro - Terminal2\MQL4\Files"
+setx PATH_T2_E "C:\Program Files (x86)\FxPro - Terminal2\MQL4\Experts"
+setx PATH_T2_I "C:\Program Files (x86)\FxPro - Terminal2\MQL4\Include"
+
+setx PATH_T3 "C:\Program Files (x86)\FxPro - Terminal3\MQL4\Files"
+setx PATH_T3_E "C:\Program Files (x86)\FxPro - Terminal3\MQL4\Experts"
+setx PATH_T3_I "C:\Program Files (x86)\FxPro - Terminal3\MQL4\Include"
+
+setx PATH_T4 "C:\Program Files (x86)\FxPro - Terminal4\MQL4\Files"
+setx PATH_T4_E "C:\Program Files (x86)\FxPro - Terminal4\MQL4\Experts"
+setx PATH_T4_I "C:\Program Files (x86)\FxPro - Terminal4\MQL4\Include"
+
+
